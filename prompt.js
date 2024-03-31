7ead517c5fca67e1fb5deb21e0baec04323d3630
Noorshah002 <155747087+Noorshah002@users.noreply.github.com>
2024-03-31 01:25:18 -0700

    prompt

---
+++ prompt.js
@@ -0,0 +1,18 @@
+
+
+
+
+let firstname= prompt(" enter your first name");
+
+console.log("firstname");
+
+
+
+let middlename= prompt(" enter your middle name");
+
+console.log("middlename");
+
+
+let lastname= prompt("enter your last name");
+
+console.log("lastname");
