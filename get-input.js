b41145792c2ed4e54ec3e4fd90741bdec2a288da
Noorshah002 <155747087+Noorshah002@users.noreply.github.com>
2024-03-31 01:48:24 -0700

    get-input

---
+++ get-input.js
@@ -0,0 +1,47 @@
+let percentage= prompt(" enter your percentage");
+
+
+
+
+
+if(percentage>=90 && percentage <=100)
+{
+    console.log("Grade A+1")
+    Grade = "A+1"
+}
+else if(percentage>=80 && percentage <=89)
+{
+    console.log("Grade A1")
+    Grade = "A1"
+}
+else if (percentage >= 70 && percentage <= 79) 
+{
+    console.log("Grade A")
+    Grade = "A"
+}
+else if (percentage >= 60 && percentage <= 69)
+ {
+    console.log("Grade B")
+    Grade = "B"
+}
+else if (percentage >= 50 && percentage <= 59)
+ {
+    console.log("Grade C")
+    Grade = "C"
+
+}
+else if (percentage >= 40 && percentage <= 49)
+ {
+    console.log("Grade D")
+    Grade = "D"
+}
+else if (percentage <34)
+ {
+    console.log("Fail")
+    Grade = "Fail"
+
+}
+else
+{
+    console.log( "Incorrect Input")
+}
